7bcbe968fde212403bcc69e9b04f612f2e8fbf8d
Javon <javondavis@users.noreply.github.com>
2019-03-28 00:13:11 -0500

    update app to use PR permissions

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
+  app.on('*', async context => {
+    context.log({ event: context.event, action: context.payload.action })
+  })
+
+  // to test: node_modules/.bin/probot receive -e issues -p test/fixtures/issues.opened.json ./index.js
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    const issueComment = context.issue({ body: 'Thanks for opening the issue' })
     return context.github.issues.createComment(issueComment)
   })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on('pull_request_review.submitted', async context => {
+    const prComment = context.issue({ body: 'Thanks for creating the PR' })
+    return context.github.issues.createComment(prComment)
+  })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -39,8 +39,3 @@ describe('My Probot app', () => {
   })
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
